4e743700e9b310a90679b2d69960a0a2e5da5661
Tanveer984 <mohdtanveer984@gmail.com>
2022-04-01 21:29:40 +0530

    homepage

---
+++ index.js
@@ -0,0 +1,18 @@
+
+document.querySelector(".midleft").addEventListener("click",frwdToMen);
+function frwdToMen(){
+    window.location.href="men.html"  // change link as per their need...
+}
+document.querySelector(".midright").addEventListener("click",frwdToWomen);
+
+function frwdToWomen(){
+    window.location.href="Women.html" //// change link as per their need...
+}
+
+document.querySelector("#searchIcon").addEventListener("click",formd);
+function formd(){
+    //console.log("inside")
+    //write remaining function as per their need..
+    console.log(document.querySelector("#search").value)
+     
+}
